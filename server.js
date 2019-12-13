f26ef5edcee43d0d5e1fb5d739c03bde810fce1f
Preston <pres.10.nap@gmail.com>
2019-12-13 14:11:37 -0500

    having issues, will work on it through lunch

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const helmet = require('helmet')
 
+const ProjectRouter = require("./projects/project-router"),
+
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
+server.use("/api/projects", ProjectRouter);
 
 module.exports = server 
