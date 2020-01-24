029cdca9b9d754633733551c64f656553c97b36b
Preston <pres.10.nap@gmail.com>
2020-01-24 13:48:00 -0500

    worked on seeding, still isn't working, questions answered

--- data/seeds/001_projects.js
+++ data/seeds/001_projects.js
@@ -1,12 +1,14 @@
-exports.seed = function(knex) {
+exports.seed = function (knex) {
     // Deletes ALL existing entries
-    return knex('table_name').del()
+    return knex('projects').del()
       .then(function () {
         // Inserts seed entries
-        return knex('table_name').insert([
-          {id: 1, colName: 'rowValue1'},
-          {id: 2, colName: 'rowValue2'},
-          {id: 3, colName: 'rowValue3'}
+        return knex('projects').insert([
+          { id: 1, name: 'project 1', description: "This is the description for project 1", completed: 0 },
+          { id: 2, name: 'project 2', description: "This is the description for project 2", completed: 0 },
+          { id: 3, name: 'project 3', description: "This is the description for project 3", completed: 0 },
+          { id: 4, name: 'project 4', description: "This is the description for project 4", completed: 0 },
+          { id: 5, name: 'project 5', description: "This is the description for project 5", completed: 0 },
         ]);
       });
   };
